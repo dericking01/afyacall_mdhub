d5d1d6930c31edd98388494b09c053e4fed94825
muhammad.mwinchande <muhammad.mwinchande@nmbbank.co.tz>
2025-02-02 12:23:32 +0300

    feat: migrated to docker with all necessary files

--- app/User.php
+++ app/User.php
@@ -1,30 +1,29 @@
 <?php
+
 namespace App;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Hash;
 use Spatie\Permission\Traits\HasRoles;
-use Hash;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 /**
- * Class User
+ * Class User.
  *
- * @package App
  * @property string $name
  * @property string $email
  * @property string $password
  * @property string $remember_token
-*/
+ */
 class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
     use HasRoles;
 
-    protected $fillable = ['name', 'email', 'password', 'remember_token','status'];
-    
+    protected $fillable = ['name', 'email', 'password', 'remember_token', 'status'];
 
-      /**
+    /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
@@ -32,39 +31,41 @@ class User extends Authenticatable implements JWTSubject
     protected $hidden = [
         'password', 'remember_token',
     ];
-          /**
-   * Get the identifier that will be stored in the subject claim of the JWT.
-   *
-   * @return mixed
-   */
-  public function getJWTIdentifier()
-  {
-      return $this->getKey();
-  }
 
-  /**
-   * Return a key value array, containing any custom claims to be added to the JWT.
-   *
-   * @return array
-   */
-  public function getJWTCustomClaims()
-  {
-      return [];
-  }
     /**
-     * Hash password
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    /**
+     * Hash password.
+     *
      * @param $input
      */
     public function setPasswordAttribute($input)
     {
-        if ($input)
+        if ($input) {
             $this->attributes['password'] = app('hash')->needsRehash($input) ? Hash::make($input) : $input;
+        }
     }
 
-    public function doctor() {
-        return $this->hasOne('App\Model\Doctor','user_id','id');
+    public function doctor()
+    {
+        return $this->hasOne('App\Model\Doctor', 'user_id', 'id');
     }
-
-
-
 }
